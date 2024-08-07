bde26b95aa7dab062d9e4a12300e1b30de7e3358
Maria Alexandra Boros <maria.boros@napa.fi>
2024-08-07 09:48:20 +0300

    Organizing Code with UserControls

--- Controls/HeaderControl.xaml.cs
+++ Controls/HeaderControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WiredBrainCoffee.CustomersApp
+namespace WiredBrainCoffee.CustomersApp.Controls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HeaderControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HeaderControl : UserControl
     {
-        public MainWindow()
+        public HeaderControl()
         {
             InitializeComponent();
         }
